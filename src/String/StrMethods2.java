f1e5eb173db4fb6b917eeca7865d504e60fb36bf
Jacky030402 <115896998+Jacky030402@users.noreply.github.com>
2023-05-29 07:46:35 +0530

    Update StrMethods2.java

--- src/String/StrMethods2.java
+++ src/String/StrMethods2.java
@@ -3,10 +3,12 @@ package String;
 public class StrMethods2 {
     public static void main(String[] args) {
         String str = "My name is Jagannath";
+        System.out.println(str);
 
         String [] result = str.split(" ");
         for(int i = 0; i<str.length();i++) {
             System.out.println(result[i]);
+            
         }
     }
 }
